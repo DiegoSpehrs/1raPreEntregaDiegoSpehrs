5a92dd2a29cefbd7043deea162bec8dd161f3661
DiegoSpehrs <diegospehrs@gmail.com>
2023-08-01 20:58:57 -0300

    Primera pre entrega Diego Spehrs

--- src/CartsManager.js
+++ src/CartsManager.js
@@ -2,36 +2,45 @@ import fs from 'fs';
 
 class CartsManager {
     constructor(path){
-        this.path
+        this.path = path
     }
     
     async getAllCarts(){
+        try{
         if(fs.existsSync(this.path)){
             const carts = await fs.promises.readFile(this.path,'utf-8')
             return JSON.parse(carts)
         }else{
             return []
         }
+        }catch(error){
+            return error
+        }
     }
     async getOneCart(id){
         const carts = await this.getAllCarts()
-        const cart = carts.find(c=>c.id===id)
+        const cart = carts.find((c)=>c.id===id)
         return cart
     }
-    async createCart(obj){
-        const carts = this.getAllCarts()
+    async createCart(){
+    try{
+        const carts = await this.getAllCarts()
         let id
         if(!carts.length){
-            id = 1
+        id = 1
         }else{
-            id= carts[carts.length-1].id + 1
+        id= carts[carts.length-1].id + 1
         }
         const newCart = {products:[],id}
         carts.push(newCart)
         await fs.promises.readFile(this.path, JSON.stringify(carts))
         return newCart
+    }catch(error){
+            return error
+        }
     }
     async addProduct(idCart,idProduct){
+     try{   
         const carts = await this.getAllCarts()
         const cart = carts.find(c=>c.product===idCart)
         const productIndex = cart.products.findIndex(p=>p.id===idProduct)
@@ -42,6 +51,9 @@ class CartsManager {
         }
         await fs.promises.writeFile(this.path,JSON.stringify(carts))
         return cart
+     }catch(error){
+        return error
+     }  
     }
 
 }
